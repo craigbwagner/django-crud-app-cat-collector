4eb44ddb5a9265e069d1f234d6dfe70da86993f4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-24 11:54:18 -0400

    add assets and link stylesheet

--- main_app/views.py
+++ main_app/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, "home.html")
 
 
 def about(request):
