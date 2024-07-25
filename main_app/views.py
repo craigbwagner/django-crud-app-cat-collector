67239894125b84119eb4c5aa5a552de679eee28b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-25 13:49:19 -0400

    add dynamic URLs

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,6 @@
 from django.shortcuts import render
+from .models import Cat
 
-# import HttpResponse to send text-based responses
-from django.http import HttpResponse
-
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-
-cats = [
-    Cat("Lolo", "tabby", "Kinda rude.", 3),
-    Cat("Sachi", "tortoiseshell", "Looks like a turtle.", 0),
-    Cat("Fancy", "bombay", "Happy fluff ball.", 4),
-    Cat("Bonk", "selkirk rex", "Meows loudly.", 6),
-]
 
 # Create your views here.
 def home(request):
@@ -29,4 +12,10 @@ def about(request):
 
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, "cats/index.html", {"cats": cats})
+
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, "cats/detail.html", {"cat": cat})
